885fd398560abedd84b825ca7f6d8d6b5865edb4
MadQuokka <snikers25@naver.com>
2019-08-28 16:20:11 +0900

    Maxst_Carengine
    Testing inspection with ar function based on Maxst image scanning.

---
+++ Assets/02.Scripts/BtnMgr.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnMgr : MonoBehaviour
+{
+    public Canvas canvas;
+    public Canvas canvas_exit;
+
+    public GameObject Button;
+
+    bool isInfoOn;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.gameObject.SetActive(false);
+        canvas_exit.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //종료이벤트
+    //public void OnClickExit()
+    //{
+    //    Application.Quit();
+    //    Debug.Log("Exit Button Click");
+
+    //}
+    //버튼클릭 이벤트
+    public void OnClick(GameObject button)
+    {
+        switch (button.tag)
+        {
+            case "INFO":
+                if (!isInfoOn)
+                {
+                    canvas.gameObject.SetActive(true);
+                    isInfoOn = true;
+                }
+                else
+                {
+                    canvas.gameObject.SetActive(false);
+                    isInfoOn = false;
+                }
+                break;
+            case "EXIT":
+                canvas_exit.gameObject.SetActive(true);
+                break;
+            case "YES":
+                canvas_exit.gameObject.SetActive(true);
+                break;
+            case "NO":
+                canvas_exit.gameObject.SetActive(false);
+                break;
+      
+        }
+    }
+
+
+}
---
+++ Assets/02.Scripts/LoadingSceneManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    private bool canOpen = false;
+    public Animator anim;
+
+    private bool isClicked;
+
+    private void Start()
+    {
+        StartCoroutine("Load");
+        anim = GetComponent<Animator>();
+    }
+    // 로딩 스크린에서 팁 등을 알려주는 경우. 버튼을 눌러 시작합니다 등을 표시
+    // 필요없다면 canOpen을 처음부터 true로 초기화하면됨.
+    public void SetCanOpen()
+    {
+        canOpen = true;
+    }
+    // 로딩
+    IEnumerator Load()
+    {
+        if (isClicked)
+        {
+            yield return new WaitForSeconds(3.0f);
+            SceneManager.LoadScene(1);
+        }
+        //async = SceneManager.LoadSceneAsync("Main Scene"); // 열고 싶은 씬
+        //async.allowSceneActivation = false;
+
+        //while (!async.isDone)
+        //{
+        //    progress = async.progress;
+
+        //    yield return true;
+
+        //    if (canOpen)
+        //        async.allowSceneActivation = true;
+        //}
+    }
+    public void Clicked()
+    {
+        isClicked = true;
+        anim.SetBool("IsClicked", true);
+        StartCoroutine("Load");
+    }
+}
+
